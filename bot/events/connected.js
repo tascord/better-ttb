44ce899d9e1f8f6a5295e542d5cc8688bb759494
tascord <35181375+tascord@users.noreply.github.com>
2020-08-15 19:12:11 +1000

    Removed Bot Code

--- bot/events/connected.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = (client) => {
-    console.log(chalk.magenta(`Connected to account ${client.username}.`))
-	console.log(chalk.blue(`Watching channels, ${client.channels}.`))
-}
--- bot/events/message.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = (message) => {
-
-    // message.channel.send('pogger');
-    message.reply('how');
-
-}
